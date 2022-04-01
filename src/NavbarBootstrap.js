8d67862aaa66477470d67eff438cb378dac85b56
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-04-01 12:59:01 +0300

    add styles for header bootstrap

--- src/NavbarBootstrap.js
+++ src/NavbarBootstrap.js
@@ -4,20 +4,23 @@ import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
 function Navibar(){
 	return(
 		<>
-			<Navbar bg="light" expand="lg">
+			<Navbar expand="lg">
 				<Container>
-					<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<Navbar.Brand href="#home">
+					<i className="bi bi-cloud-sun-fill Logo__img" style={{ display: 'inline-flex', marginRight: '10px', fontSize: '50px', color: 'cornflowerblue'}}></i>
+						Weather in any city
+					</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#link">Link</Nav.Link>
-						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+					<Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+						<Nav className="ml-auto">
+						<Nav.Link href="#home">Сhoose City</Nav.Link>
+						<Nav.Link href="#link">Market</Nav.Link>
+						<NavDropdown title="Popular Cities" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Kharkiv</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">Kyiv</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">Viena</NavDropdown.Item>
 							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.4">Phuket</NavDropdown.Item>
 						</NavDropdown>
 						</Nav>
 					</Navbar.Collapse>

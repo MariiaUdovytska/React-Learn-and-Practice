97da1bcc764b5d69192e2e65346679926bed8e47
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-04-05 20:34:06 +0300

    add fetch for forecast and isDebag file.json

--- src/DataWeatherForecast.js
+++ src/DataWeatherForecast.js
@@ -1,13 +1,49 @@
 import React from 'react';
+import Forecast10 from './data/forecast-10.json'
+
+var isDebug = true;
 
 class DataWeatherForecast extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
+	componentDidMount() {
+		this.loadData();
+	}
+
+	mapData = (response) =>{
+		console.log(response);
+	}
+	loadData = ()=>{
+		if(isDebug){
+			this.mapData(Forecast10);
+			return;
+		}
+
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
+				'X-RapidAPI-Key': 'c497811108msh60fccf337372cb8p1f70b2jsn15aee00ffb73'
+			}
+		};
+		
+		fetch(`https://community-open-weather-map.p.rapidapi.com/forecast/daily?q=${this.props.cityName}&cnt=10&units=metric`, options)
+			.then(response => response.json())
+			.then(response => this.mapData(response))
+			.catch(err => {
+				this.setState({
+					isLoaded: true,
+					err
+				});
+			});
+	}
+
 	render(){
 		return(
-			<div></div>
+			<div className='forecast__cities'></div>
+			
 		)
 	}
 }

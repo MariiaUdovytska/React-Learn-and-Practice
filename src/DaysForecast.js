6975123d69f732b6f5cf4d30ca9536b3a2062829
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-04-08 21:42:21 +0300

    filled out info with Api for sublist DaysForecast

--- src/DaysForecast.js
+++ src/DaysForecast.js
@@ -14,7 +14,7 @@ class DaysForecast extends React.Component {
 	}
 
 	render(){
-		const {dt, weather, temp, feels_like}=this.props.informDay;
+		const {dt, weather, temp, feels_like, pressure, humidity, speed}=this.props.informDay;
 		let date = new Date(dt*1000);
 		return(
 			<div onClick={this.handleClick}>
@@ -53,8 +53,38 @@ class DaysForecast extends React.Component {
 								</div>
 							</div>
 						</div>
-						<div className='forecast__sublist-down'></div>
-						
+						<div className='forecast__sublist-center'>
+							<ul className='forecast__sublist-center'>
+								<li><span>Humidity: {humidity+"%"}</span></li>
+								<li><span>Pressure: {pressure+"hPa"}</span></li>
+								<li><span>Speed: {speed+"m/s"}</span></li>
+							</ul>
+						</div>
+						<div className='forecast__sublist-down'>
+							<table className='forecast__sublist-down-table'>
+								<tr className='forecast__sublist-down-table-row'>
+									<th className='forecast__sublist-down-table-column'></th>
+									<th className='forecast__sublist-down-table-column'>Morning</th>
+									<th className='forecast__sublist-down-table-column'>Afternoon</th>
+									<th className='forecast__sublist-down-table-column'>Evening</th>
+									<th className='forecast__sublist-down-table-column'>Night</th>
+								</tr>
+								<tr className='forecast__sublist-down-table-row'>
+									<td className='forecast__sublist-down-table-temp'>Temperature</td>
+									<td className='forecast__sublist-down-table-temp'>{temp.morn}°C</td>
+									<td className='forecast__sublist-down-table-temp'>{temp.day}°C</td>
+									<td className='forecast__sublist-down-table-temp'>{temp.eve}°C</td>
+									<td className='forecast__sublist-down-table-temp'>{temp.night}°C</td>
+								</tr>
+								<tr className='forecast__sublist-down-table-row'>
+									<td className='forecast__sublist-down-table-feels'>Feels like</td>
+									<td className='forecast__sublist-down-table-feels'>{feels_like.morn}°C</td>
+									<td className='forecast__sublist-down-table-feels'>{feels_like.day}°C</td>
+									<td className='forecast__sublist-down-table-feels'>{feels_like.eve}°C</td>
+									<td className='forecast__sublist-down-table-feels'>{feels_like.night}°C</td>
+								</tr>
+							</table>
+						</div>
 					</div>
 					: ""
 				}

9256bbfbb786acce3f13d12df3d2518422869a1d
MariiaUdovytska <mariiaudovytska06@gmail.com>
2022-04-11 19:46:18 +0300

    change in headerBoots fjr working collapse menu where click on item

--- src/NavbarBootstrap.js
+++ src/NavbarBootstrap.js
@@ -1,23 +1,26 @@
-import React from "react"
-import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap';
+import React, { useState } from "react"
+import {Navbar, Nav, Container} from 'react-bootstrap';
 import {Link} from "react-router-dom";
 
 
+
 function Navibar(props){
+	const [expanded, setExpanded] = useState(false);
+
 	return(
 		<>
-			<Navbar expand="lg">
+			<Navbar expanded={expanded} expand="lg">
 				<Container>
 					<Navbar.Brand href="#home">
 						<i className="bi bi-cloud-sun-fill Logo__img" style={{ display: 'inline-flex', marginRight: '10px', fontSize: '50px', color: 'cornflowerblue'}}></i>
 						{props.firstHeader.name}
 					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
 						<Nav className="ml-auto">
-						<Nav.Link><Link className="navbar__a" to="/">Dashboard</Link></Nav.Link>
-						<Nav.Link><Link className="navbar__a" to="/forecast">10-day forecast</Link></Nav.Link>
-						<Nav.Link><Link className="navbar__a" to="/about">About</Link></Nav.Link>
+							<Nav.Link onClick={() => setExpanded(false)}><Link className="navbar__a" to="/">Dashboard</Link></Nav.Link>
+							<Nav.Link onClick={() => setExpanded(false)}><Link className="navbar__a" to="/forecast">10-day forecast</Link></Nav.Link>
+							<Nav.Link onClick={() => setExpanded(false)}><Link className="navbar__a" to="/about">About</Link></Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
